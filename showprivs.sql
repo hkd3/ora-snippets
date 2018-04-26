fc2977083db476bea61975942ae60f5cc4c70414
hkd3 <masakim8192@gmail.com>
2018-04-26 15:41:05 +0900

    added upper() and improved output format

--- showprivs.sql
+++ showprivs.sql
@@ -1,11 +1,24 @@
 set pages 9999 lin 100
-col grantee for a10
-col owner for a10
-col table_name for a10
-col grantor for a10
-col privilege for a30
-col granted_role for a20
-col type for a10
-select * from dba_sys_privs where grantee like '&1' order by grantee, privilege;
-select * from dba_tab_privs where grantee like '&1' order by grantee, privilege;
-select * from dba_role_privs where grantee like '&1' order by grantee, granted_role;
+col grantee for a30
+col privilege for a40
+select * from dba_sys_privs
+where grantee like upper('&1')
+order by grantee, privilege;
+
+set pages 9999 lin 180
+col grantee for a20
+col owner for a20
+col table_name for a30
+col grantor for a20
+col privilege for a40
+col type for a20
+select * from dba_tab_privs
+where grantee like upper('&1')
+order by grantee, privilege;
+
+set pages 9999 lin 100
+col grantee for a30
+col granted_role for a30
+select * from dba_role_privs
+where grantee like upper('&1')
+order by grantee, granted_role;
