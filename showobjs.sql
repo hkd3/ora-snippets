1ddd427f014e6918cc8af146fb6922c58ed093db
hkd3 <masakim8192@gmail.com>
2018-04-26 17:48:41 +0900

    added upper()

--- showobjs.sql
+++ showobjs.sql
@@ -2,4 +2,7 @@ set pages 9999 lin 100
 col owner for a30
 col object_name for a30
 col object_type for a20
-select owner, object_name, object_type from dba_objects where owner like '%&1%' order by 1,3,2;
+select owner, object_name, object_type
+from dba_objects
+where owner like upper('%&1%')
+order by owner, object_type, object_name;
